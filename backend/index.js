ef762a06fedc7a3ec7da175a1628531b48f4294f
Omkar <jadhavpatil2999@gmail.com>
2023-07-05 16:22:28 +0530

    Initial

---
+++ backend/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const Moralis = require("moralis").default;
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = 3001;
+const ABI = require("./abi.json");
+
+app.use(cors());
+app.use(express.json());
+
+function convertArrayToObjects(arr) {
+  const dataArray = arr.map((transaction, index) => ({
+    key: (arr.length + 1 - index).toString(),
+    type: transaction[0],
+    amount: transaction[1],
+    message: transaction[2],
+    address: `${transaction[3].slice(0, 4)}...${transaction[3].slice(38)}`,
+    subject: transaction[4],
+  }));
+  return dataArray.reverse();
+}
+
+app.get("/getNameAndBalance", async (req, res) => {
+  const { userAddress } = req.query;
+
+  const response = await Moralis.EvmApi.utils.runContractFunction({
+    chain: "0x13881",
+    address: "0x95495eEF55D7aDF414a0829158dC83D002C374e6",
+    functionName: "getMyName",
+    abi: ABI,
+    params: { _user: userAddress },
+  });
+
+  const jsonResponseName = response.raw;
+
+  const secResponse = await Moralis.EvmApi.balance.getNativeBalance({
+    chain: "0x13881",
+    address: userAddress,
+  });
+
+  const jsonResponsebal = (secResponse.raw.balance / 1e18).toFixed(2);
+
+  const thirResponse = await Moralis.EvmApi.token.getTokenPrice({
+    address: "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0",
+  });
+
+  const jsonResponseDollars = (
+    thirResponse.raw.usdPrice * jsonResponsebal
+  ).toFixed(2);
+
+  const fourResponse = await Moralis.EvmApi.utils.runContractFunction({
+    chain: "0x13881",
+    address: "0x95495eEF55D7aDF414a0829158dC83D002C374e6",
+    functionName: "getHistory",
+    abi: ABI,
+    params: { _user: userAddress },
+  });
+
+  const jsonResponseHistory = convertArrayToObjects(fourResponse.raw);
+
+  const fiveResponse = await Moralis.EvmApi.utils.runContractFunction({
+    chain: "0x13881",
+    address: "0x95495eEF55D7aDF414a0829158dC83D002C374e6",
+    functionName: "getMyRequests",
+    abi: ABI,
+    params: { _user: userAddress },
+  });
+
+  const jsonResponseRequests = fiveResponse.raw;
+
+  const jsonResponse = {
+    name: jsonResponseName,
+    balance: jsonResponsebal,
+    dollars: jsonResponseDollars,
+    history: jsonResponseHistory,
+    requests: jsonResponseRequests,
+  };
+
+  return res.status(200).json(jsonResponse);
+});
+
+Moralis.start({
+  apiKey: process.env.MORALIS_KEY,
+}).then(() => {
+  app.listen(port, () => {
+    console.log(`Listening for API Calls`);
+  });
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,125 @@
+import "./App.css";
+import logo from "./logo.png";
+import { Layout, Button } from "antd";
+import CurrentBalance from "./componets/CurrentBalance";
+import RequestAndPay from "./componets/RequestAndPay";
+import AccountDetails from "./componets/AccountDetails";
+import RecentActivity from "./componets/RecentActivity";
+import { useConnect, useAccount, useDisconnect } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const { Header, Content } = Layout;
+
+function App() {
+  const [name, setName] = useState("...");
+  const [balance, setBalance] = useState("...");
+  const [dollars, setDollars] = useState("...");
+  const [history, setHistory] = useState(null);
+  const [requests, setRequests] = useState({ 1: [0], 0: [] });
+
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { connect } = useConnect({
+    connector: new MetaMaskConnector(),
+  });
+
+  function disconnectAndSetNull() {
+    disconnect();
+
+    setName("...");
+    setBalance("...");
+    setDollars("...");
+    setHistory(null);
+    setRequests({ 1: [0], 0: [] });
+  }
+
+  async function getNameAndBalance() {
+    const res = await axios.get(`http://localhost:3001/getNameAndBalance`, {
+      params: { userAddress: address },
+    });
+
+    const response = res.data;
+    console.log(response);
+
+    if (response.name[1]) {
+      setName(response.name[0]);
+    }
+    setBalance(response.balance);
+    setDollars(response.dollars);
+    setHistory(response.history);
+    setRequests(response.requests);
+  }
+
+  useEffect(() => {
+    if (!isConnected) return;
+    getNameAndBalance();
+  }, [isConnected]);
+
+  return (
+    <div className="App">
+      <Layout>
+        <Header className="header">
+          <div className="headerLeft">
+            <img src={logo} alt="logo" className="logo" />
+            {isConnected && (
+              <>
+                <div
+                  className="menuOption"
+                  style={{ borderBottom: "1.5px solid black" }}
+                >
+                  Summary
+                </div>
+                <div className="menuOption">Activity</div>
+                <div className="menuOption">{`Send & Request`}</div>
+                <div className="menuOption">Wallet</div>
+                <div className="menuOption">Help</div>
+              </>
+            )}
+          </div>
+
+          {isConnected ? (
+            <Button type={"primary"} onClick={disconnectAndSetNull}>
+              Disconnect Wallet
+            </Button>
+          ) : (
+            <Button
+              type={"primary"}
+              onClick={() => {
+                connect();
+              }}
+            >
+              Connect Wallet
+            </Button>
+          )}
+        </Header>
+        <Content className="content">
+          {isConnected ? (
+            <>
+              <div className="firstColumn">
+                <CurrentBalance dollars={dollars} />
+                <RequestAndPay
+                  requests={requests}
+                  getNameAndBalance={getNameAndBalance}
+                />
+                <AccountDetails
+                  address={address}
+                  name={name}
+                  balance={balance}
+                />
+              </div>
+              <div className="secondColumn">
+                <RecentActivity history={history} />
+              </div>
+            </>
+          ) : (
+            <div>Please login</div>
+          )}
+        </Content>
+      </Layout>
+    </div>
+  );
+}
+
+export default App;
